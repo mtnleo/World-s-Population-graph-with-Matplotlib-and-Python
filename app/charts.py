341b36420b7876910a8ea5ec72076108495f4adb
mtnleo <118694913+mtnleo@users.noreply.github.com>
2022-12-02 14:16:43 +0000

    - I added a menu to choose between a bar and a pie chart. - I optimized the code with more functions. - I changed the way of creating the labels and values list, using map and filter.

--- app/charts.py
+++ app/charts.py
@@ -1,17 +1,54 @@
 import matplotlib.pyplot as plt
 import read_csv as rcsv
 
-def show_area_countries(countries_list):
+
+def get_values_labels(countries_list):
+
+  labels = list(filter(lambda count: int(count["Rank"]) < 10, countries_list))
+  labels = list(map(lambda count: count["Country/Territory"], labels))
+
+  values = list(filter(lambda count: int(count["Rank"]) < 10, countries_list))
+  values = list(map(lambda count: count["2022 Population"], values))
+
+  return labels, values
+
+
+def show_population_countries_bars(countries_list):
   fig, ax = plt.subplots()
 
-  labels = [name["Country/Territory"] for name in countries_list if int(name["Rank"]) < 10]
-  values = [int(area["2022 Population"]) for area in countries_list if int(area["Rank"]) < 10]
+  values = []
+  labels = []
+
+  labels, values = get_values_labels(countries_list)
 
   ax.bar(labels, values)
   plt.show()
 
+
+def show_population_countries_pie(countries_list):
+  fig, ax = plt.subplots()
+
+  values = []
+  labels = []
+  
+  labels, values = get_values_labels(countries_list)
+
+  ax.pie(values, labels=labels)
+  ax.axis("equal")
+  plt.show()
+
+
 if __name__ == "__main__":
   popl_list = rcsv.read_csv("./app/data.csv")
   # Graph by area
-  show_area_countries(popl_list)
+  choice = 0
+  while choice != 1 and choice != 2:
+    choice = int(
+      input("What do you want to see?\n| 1 |  Bar Chart\n| 2 |  Pie Chart\n"))
+    if (choice != 1 and choice != 2):
+      print("| X | SELECT A VALID OPTION | X |")
 
+  if choice == 1:
+    show_population_countries_bars(popl_list)
+  else:
+    show_population_countries_pie(popl_list)
